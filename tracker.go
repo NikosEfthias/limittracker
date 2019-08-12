606c91eee63bd103ff1e4471612a5441999e8a4a
nikos efthias <nikos@mugsoft.io>
2019-08-13 00:10:48 +0300

    fixes

--- tracker.go
+++ tracker.go
@@ -1,6 +1,7 @@
 package ratetracker
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
@@ -9,7 +10,6 @@ import (
 type Bucket struct {
 	l        sync.Mutex
 	entries  []time.Time
-	tick     time.Time
 	duration time.Duration
 }
 
@@ -25,15 +25,14 @@ func (b *Bucket) Len() int {
 func (b *Bucket) Add() {
 	b.l.Lock()
 	defer b.l.Unlock()
-	checkAndDeleteOld(b)
 	b.entries = append(b.entries, time.Now())
+	checkAndDeleteOld(b)
 }
 
 //NewBucket creates a new bucket with a timeframe given in seconds
 func NewBucket(duration time.Duration) *Bucket {
 	return &Bucket{
 		entries:  make([]time.Time, 0, 100),
-		tick:     time.Now(),
 		duration: duration,
 	}
 }
@@ -83,25 +82,18 @@ func (m *BucketMap) Len(key string) int {
 func checkAndDeleteOld(b *Bucket) {
 	//this function assumes the bucket is locked elsewhere. If the bucket is not locked before being passed in this function
 	//it will likely cause a race condition
-	if b.tick.Unix() <= time.Now().Add(-1*b.duration).Unix() {
-		//if last tick was earlier than current time minus the specified duration  set the tick to current time
-		b.tick = time.Now()
-	}
 	if len(b.entries) == 0 {
 		return
 	}
 
-	offset := 0
+	offset := -1
 	for i, v := range b.entries {
-		if v.Unix() <= b.tick.Unix() {
+		if v.Unix() < time.Now().Add(-1*b.duration).Unix() {
 			offset = i
+			fmt.Println("old data")
 		} else {
 			break
 		}
 	}
-	if len(b.entries)-1 > offset {
-		b.entries = b.entries[offset:]
-	} else {
-		b.entries = []time.Time{}
-	}
+	b.entries = b.entries[offset+1:]
 }
