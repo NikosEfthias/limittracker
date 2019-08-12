aa2ac7eee5c8c1d8d5c6facfc962897bdfe5ebd7
nikos efthias <nikos@mugsoft.io>
2019-08-12 22:12:37 +0300

    add

--- tracker.go
+++ tracker.go
@@ -7,24 +7,42 @@ import (
 
 //Bucket is simply a bucket
 type Bucket struct {
-	l        sync.RWMutex
+	l        sync.Mutex
 	entries  []time.Time
 	tick     time.Time
 	duration time.Duration
 }
 
 //Len returns the number of entries specified in the
-func (bucket *Bucket) Len() int {
-	bucket.l.Lock()
-	defer bucket.l.Unlock()
-	checkAndDeleteOld(bucket)
-	//if last tick was earlier than 15 mins ago then set the tick to current time
-	return len(bucket.entries)
+func (b *Bucket) Len() int {
+	b.l.Lock()
+	defer b.l.Unlock()
+	checkAndDeleteOld(b)
+	return len(b.entries)
 }
+
+//Add adds new entry to the list
+func (b *Bucket) Add() {
+	b.l.Lock()
+	defer b.l.Unlock()
+	checkAndDeleteOld(b)
+	b.entries = append(b.entries, time.Now())
+}
+
+//NewBucket creates a new bucket with a timeframe given in seconds
+func NewBucket(duration time.Duration) *Bucket {
+	return &Bucket{
+		entries:  make([]time.Time, 0, 100),
+		tick:     time.Now(),
+		duration: duration,
+	}
+}
+
 func checkAndDeleteOld(b *Bucket) {
 	//this function assumes the bucket is locked elsewhere. If the bucket is not locked before being passed in this function
 	//it will likely cause a race condition
 	if b.tick.Unix() <= time.Now().Add(-1*b.duration).Unix() {
+		//if last tick was earlier than current time minus the specified duration  set the tick to current time
 		b.tick = time.Now()
 	}
 
@@ -37,13 +55,4 @@ func checkAndDeleteOld(b *Bucket) {
 		}
 	}
 	b.entries = b.entries[offset:]
-}
-
-//NewBucket creates a new bucket with a timeframe given in seconds
-func NewBucket(duration time.Duration) *Bucket {
-	return &Bucket{
-		entries:  make([]time.Time, 0, 100),
-		tick:     time.Now(),
-		duration: duration,
-	}
 }
